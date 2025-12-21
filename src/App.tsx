0529dbf4487966874ece19f066b58f5c385b2206
Ibrahim <ibrahim.hassan2402@gmail.com>
2025-12-21 19:01:47 +0000

    Voxel and ShapeRenderer components initialised

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
+
+export interface Voxel {
+  type: 'cube' | 'ramp' | 'half';
+  rotation?: [number, number, number];
+};
+
+// VoxelSet tracks which voxels are active to make up the 2x2x2 composite shape.
+export type VoxelSpace = {
+  0: null|Voxel;
+  1: null|Voxel;
+  2: null|Voxel;
+  3: null|Voxel;
+  4: null|Voxel;
+  5: null|Voxel;
+  6: null|Voxel;
+  7: null|Voxel;
+};
 
 const App = () => {
+
+// Start with an empty voxelSpace. Use setVoxelSpace when making changes to the scene.
+const [voxelSpace, setVoxelSpace] = useState<VoxelSpace>({
+    0: null, 1: null, 2: null, 3: null, 
+    4: null, 5: null, 6: null, 7: null
+});
+
   return (
-    <div className="h-dvh">
-      <Canvas frameloop={"demand"} camera={ {position: [2,2,2]} }>
+    <div className="h-dvh bg-gray-500">
+      <Canvas 
+        frameloop={"demand"} 
+        camera={ {zoom: 135, position: [3,3,3]} } 
+        orthographic
+        onPointerDown={toggleCube}
+      >
         <axesHelper args={[2]}/>
+        <ambientLight intensity={1.5} />
+
         <OrbitControls/>
       </Canvas>
     </div>
---
+++ src/components/Voxel.tsx
@@ -0,0 +1,30 @@
+import { type Voxel as VoxelData } from '../App';
+
+/* Voxel from App.tsx (read in here as VoxelType) tells this component
+what type of voxel (cube, ramp, etc) to construct, alongside position and rotation. */
+interface VoxelProps {
+  position: [number, number, number];
+  type: VoxelData['type'];
+  rotation?: VoxelData['rotation']
+}
+
+/* All actual R3F implementations of shape geometry go here and are referenced
+when the type passed in as param matches. */
+const GEOMETRIES = {
+    cube: <boxGeometry args={[1,1,1]} />,
+    half: <boxGeometry args={[1,0.5,1]} />,
+    ramp: null
+};
+
+const Voxel = ({ position, type, rotation=[0,0,0] }: VoxelProps) => {
+  return (
+    <group position={position} rotation={rotation}>
+        <mesh>
+            {GEOMETRIES[type]}
+            <meshStandardMaterial color={"white"} /> {/* All shapes are this standard white, outlined post-render. */}
+        </mesh>
+    </group>
+  );
+};
+
+export default Voxel;
