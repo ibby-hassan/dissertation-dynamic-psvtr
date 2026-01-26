f6e76ee999ff2611f2a6356400c628bddcaa4576
Ibrahim <ibrahim.hassan2402@gmail.com>
2026-01-26 15:42:32 +0000

    Fixed a bug with half-subshapes

--- src/components/ShapeSpace.tsx
+++ src/components/ShapeSpace.tsx
@@ -6,8 +6,16 @@ interface ShapeSpaceProps {
 
 const GEOMETRIES = {
   empty: null,
-  cube: <boxGeometry args={[1, 1, 1]} />,
-  half: <boxGeometry args={[1, 0.5, 1]} />,
+  cube: {
+    geometry: <boxGeometry args={[1, 1, 1]} />,
+    offset: [0, 0, 0] as [number, number, number],
+    color: 'white',
+  },
+  half: {
+    geometry: <boxGeometry args={[1, 0.5, 1]} />,
+    offset: [0, -0.25, 0] as [number, number, number],
+    color: 'white',
+  },
   wedge: null
 };
 
@@ -15,15 +23,16 @@ const ShapeSpace = ({ shape }: ShapeSpaceProps) => {
   return (
     <group position={[-0.5, -0.5, -0.5]}>
       {shape.map((subshape: Subshape) => {
-        if (subshape.type === 'empty') return null;
 
         const { position, rotation, type, index } = subshape;
+        const geometry = GEOMETRIES[type];
+        if (!geometry) return null;
 
         return (
           <group key={index} position={position} rotation={rotation}>
-            <mesh>
-              {GEOMETRIES[type]}
-              <meshStandardMaterial color="white" />
+            <mesh position={geometry.offset}>
+              {geometry.geometry}
+              <meshStandardMaterial color={geometry.color} />
             </mesh>
           </group>
         );
